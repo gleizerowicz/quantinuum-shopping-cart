883e224a2ddb27938459a13f9fd5b4aaf17e9c98
Chad Thomas <chad.c.thomas@gmail.com>
2017-04-02 16:59:11 -0400

    re-init solution

---
+++ ShoppingCartService/HomeModule.cs
@@ -0,0 +1,12 @@
+namespace ShoppingCartService
+{
+    using Nancy;
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello from Nancy running on CoreCLR");
+        }
+    }
+}
